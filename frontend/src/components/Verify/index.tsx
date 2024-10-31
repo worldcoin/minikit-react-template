7c91cab054f64dd3ac703f6ca06ff4d1fac62528
Michal Struck <michal.o.struck@gmail.com>
2024-10-31 12:18:29 +0100

    feat: update template to use async handlers

--- frontend/src/components/Verify/index.tsx
+++ frontend/src/components/Verify/index.tsx
@@ -1,11 +1,11 @@
 import {
   MiniKit,
-  ResponseEvent,
   VerificationLevel,
-  MiniAppVerifyActionPayload,
   ISuccessResult,
+  MiniAppVerifyActionErrorPayload,
+  IVerifyResponse,
 } from "@worldcoin/minikit-js";
-import { useEffect } from "react";
+import { useCallback, useState } from "react";
 
 export type VerifyCommandInput = {
   action: string;
@@ -19,58 +19,63 @@ const verifyPayload: VerifyCommandInput = {
   verification_level: VerificationLevel.Orb, // Orb | Device
 };
 
-const triggerVerify = () => {
-  MiniKit.commands.verify(verifyPayload);
-};
-
 export const VerifyBlock = () => {
-  useEffect(() => {
+  const [handleVerifyResponse, setHandleVerifyResponse] = useState<
+    MiniAppVerifyActionErrorPayload | IVerifyResponse | null
+  >(null);
+
+  const handleVerify = useCallback(async () => {
     if (!MiniKit.isInstalled()) {
-      return;
+      console.warn("Tried to invoke 'verify', but MiniKit is not installed.");
+      return null;
     }
 
-    MiniKit.subscribe(
-      ResponseEvent.MiniAppVerifyAction,
-      async (response: MiniAppVerifyActionPayload) => {
-        if (response.status === "error") {
-          return console.log("Error payload", response);
-        }
+    const { finalPayload } = await MiniKit.commandsAsync.verify(verifyPayload);
 
-        // Verify the proof in the backend
-        const verifyResponse = await fetch(
-          `${process.env.NEXTAUTH_URL}/api/verify`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              payload: response as ISuccessResult, // Parses only the fields we need to verify
-              action: verifyPayload.action,
-              signal: verifyPayload.signal, // Optional
-            }),
-          }
-        );
+    // no need to verify if command errored
+    if (finalPayload.status === "error") {
+      console.log("Command error");
+      console.log(finalPayload);
 
-        // TODO: Handle Success!
-        const verifyResponseJson = await verifyResponse.json();
-        if (verifyResponseJson.status === 200) {
-          console.log("Verification success!");
-        }
+      setHandleVerifyResponse(finalPayload);
+      return finalPayload;
+    }
+
+    // Verify the proof in the backend
+    const verifyResponse = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/verify`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
+          action: verifyPayload.action,
+          signal: verifyPayload.signal, // Optional
+        }),
       }
     );
 
-    return () => {
-      MiniKit.unsubscribe(ResponseEvent.MiniAppVerifyAction);
-    };
+    // TODO: Handle Success!
+    const verifyResponseJson = await verifyResponse.json();
+
+    if (verifyResponseJson.status === 200) {
+      console.log("Verification success!");
+      console.log(finalPayload);
+    }
+
+    setHandleVerifyResponse(verifyResponseJson);
+    return verifyResponseJson;
   }, []);
 
   return (
     <div>
       <h1>Verify Block</h1>
-      <button className="bg-green-500 p-4" onClick={triggerVerify}>
+      <button className="bg-green-500 p-4" onClick={handleVerify}>
         Test Verify
       </button>
+      <span>{JSON.stringify(handleVerifyResponse, null, 2)}</span>
     </div>
   );
 };
