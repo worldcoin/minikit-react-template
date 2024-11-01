ebe34bf59c4673f28557ebdd8a79bde8cc8588d1
Michał Struck <98230431+michalstruck@users.noreply.github.com>
2024-11-01 19:49:39 +0100

    Async template update (#3)
    * feat: update template to use async handlers
    
    * bump minikit in backend
    
    * chore: bump minikit

--- frontend/src/components/Pay/index.tsx
+++ frontend/src/components/Pay/index.tsx
@@ -3,79 +3,77 @@ import {
   tokenToDecimals,
   Tokens,
   PayCommandInput,
-  ResponseEvent,
-  MiniAppPaymentPayload,
 } from "@worldcoin/minikit-js";
-import { useEffect } from "react";
 
 const sendPayment = async () => {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/initiate-payment`, {
-    method: "POST",
-  });
-
-  const { id } = await res.json();
-
-  console.log(id);
-
-  const payload: PayCommandInput = {
-    reference: id,
-    to: "0x0c892815f0B058E69987920A23FBb33c834289cf", // Test address
-    tokens: [
+  try {
+    const res = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/initiate-payment`,
       {
-        symbol: Tokens.WLD,
-        token_amount: tokenToDecimals(0.5, Tokens.WLD).toString(),
-      },
-      {
-        symbol: Tokens.USDCE,
-        token_amount: tokenToDecimals(0.1, Tokens.USDCE).toString(),
-      },
-    ],
-    description: "Watch this is a test",
-  };
+        method: "POST",
+      }
+    );
 
-  if (MiniKit.isInstalled()) {
-    MiniKit.commands.pay(payload);
+    const { id } = await res.json();
+
+    console.log(id);
+
+    const payload: PayCommandInput = {
+      reference: id,
+      to: "0x0c892815f0B058E69987920A23FBb33c834289cf", // Test address
+      tokens: [
+        {
+          symbol: Tokens.WLD,
+          token_amount: tokenToDecimals(0.5, Tokens.WLD).toString(),
+        },
+        {
+          symbol: Tokens.USDCE,
+          token_amount: tokenToDecimals(0.1, Tokens.USDCE).toString(),
+        },
+      ],
+      description: "Watch this is a test",
+    };
+    if (MiniKit.isInstalled()) {
+      return await MiniKit.commandsAsync.pay(payload);
+    }
+    return null;
+  } catch (error: unknown) {
+    console.log("Error sending payment", error);
+    return null;
+  }
+};
+
+const handlePay = async () => {
+  if (!MiniKit.isInstalled()) {
+    console.error("MiniKit is not installed");
+    return;
+  }
+  const sendPaymentResponse = await sendPayment();
+  const response = sendPaymentResponse?.finalPayload;
+  if (!response) {
+    return;
+  }
+
+  if (response.status == "success") {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/confirm-payment`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ payload: response }),
+    });
+    const payment = await res.json();
+    if (payment.success) {
+      // Congrats your payment was successful!
+      console.log("SUCESS!");
+    } else {
+      // Payment failed
+      console.log("FAILED!");
+    }
   }
 };
 
 export const PayBlock = () => {
-  useEffect(() => {
-    if (!MiniKit.isInstalled()) {
-      console.error("MiniKit is not installed");
-      return;
-    }
-
-    MiniKit.subscribe(
-      ResponseEvent.MiniAppPayment,
-      async (response: MiniAppPaymentPayload) => {
-        if (response.status == "success") {
-          const res = await fetch(
-            `${process.env.NEXTAUTH_URL}/api/confirm-payment`,
-            {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ payload: response }),
-            }
-          );
-          const payment = await res.json();
-          if (payment.success) {
-            // Congrats your payment was successful!
-            console.log("SUCESS!");
-          } else {
-            // Payment failed
-            console.log("FAILED!");
-          }
-        }
-      }
-    );
-
-    return () => {
-      MiniKit.unsubscribe(ResponseEvent.MiniAppPayment);
-    };
-  }, []);
-
   return (
-    <button className="bg-blue-500 p-4" onClick={sendPayment}>
+    <button className="bg-blue-500 p-4" onClick={handlePay}>
       Pay
     </button>
   );
